248e437073301a87651d3d5bb1abee708aa1475b
hitarthaGIT <152644627+hitarthaGIT@users.noreply.github.com>
2024-02-26 14:01:09 +0530

    Add files via upload

--- insert_end.py
+++ insert_end.py
@@ -7,3 +7,10 @@ word=input("Enter the word to be inserted : ")
 str=str1.split()
 str.append(word)
 print(str)
+
+'''
+Output:
+Enter the word to be inserted : Bye!!
+['My', 'name', 'is', 'Hitartha', 'and', 'I', 'love', 'coding', '!!','Bye!!']
+
+'''
